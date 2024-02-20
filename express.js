ffebeea92057f5efac428de99593b4aca2df2265
SalahAnnuka <158805101+SalahAnnuka@users.noreply.github.com>
2024-02-20 13:46:10 +0300

    Update express.js

--- express.js
+++ express.js
@@ -8,34 +8,27 @@ app.get('/',function(req,res){
 });
 
 var fact = (fc)=>{return fc==0? 1 : fc*fact(fc-1)};
-var fib = (fb)=>{return fb==0? 0 : fb+fib(fb-1)};
+var add = (ad)=>{return ad==0? 0 : ad+add(ad-1)};
 var multi = (ml)=>{
     var arr = {};
-    for(var i=1 ; i<=10 ; i++)
-    {
+    for(var i=1 ; i<=10 ; i++){
         arr[i-1] = ml + " * " + i + " = " + (i*ml);
     }
-    return arr;
-};
+    return arr;};
 
-var pal = (pl)=>
-{
+var pal = (pl)=>{
     var pld = true;
     var start = 0, end = pl.length-1;
-    console.log(pl[pl.length-1]);
     while (start<end){
         if (pl[start] != pl[end])
         {
             pld = false;
             break;
         }
-        console.log(pl[start]);
-        console.log(pl[end]);
         start++;
         end--;
     }
-    return pld? 'palindrome' : 'not palindrome';
-}
+    return pld? 'palindrome' : 'not palindrome';};
 
 app.get('/fact/:num',function(req,res){
     var result = fact(req.params.num);
@@ -43,10 +36,10 @@ app.get('/fact/:num',function(req,res){
     res.json('factorial for '+req.params.num+' is: '+ result);
 });
 
-app.get('/fib/:num',function(req,res){
-    var result = fib(req.params.num);
+app.get('/add/:num',function(req,res){
+    var result = add(req.params.num);
     console.log(result);
-    res.json('fibonacci for '+req.params.num+' is: '+ result);
+    res.json('Cumulative sum for '+req.params.num+' is: '+ result);
 });
 
 app.get('/multi/:num',function(req,res){
